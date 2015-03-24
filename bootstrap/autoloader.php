42aa2a92eee9cb955d6e0c08d6cedd5baa5fe1c0
Tobias Negd <wu3rstle@gmx.de>
2015-03-24 14:27:05 +0100

    moved autoload from autoloader.php to composer.json

--- bootstrap/autoloader.php
+++ bootstrap/autoloader.php
@@ -6,19 +6,6 @@
  * Time: 16:34
  */
 
-function __autoLoad($classname) {
-	if (file_exists('app/models/'.$classname.'.php')) {
-		require_once 'app/models/'.$classname.'.php';
-	} else if (file_exists('app/controllers/'.$classname.'.php')) {
-		require_once 'app/controllers/'.$classname.'.php';
-	} else if (file_exists('app/middlewares/'.$classname.'.php')) {
-		require_once 'app/middlewares/'.$classname.'.php';
-	} else if (file_exists('helpers/'.$classname.'.php')) {
-		require_once 'helpers/'.$classname.'.php';
-	}
-}
-spl_autoload_register('__autoLoad');
-
 $config = array();
 foreach (glob('config/*.php') as $file) {
 	$config[basename($file, '.php')] = include_once $file;
